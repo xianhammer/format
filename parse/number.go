d22c8f820f747d841a24c8fcb2ab6b26032262f0
Christian Hammer <christian_hammer@hotmail.com>
2020-03-30 10:29:49 +0200

    Parsing fixes, speed-up and additions.

--- parse/number.go
+++ parse/number.go
@@ -4,13 +4,29 @@ import (
 	"math"
 )
 
-// Integer parses all decimal digits from start of the given byte slice.
+// Hex parses all hexadecimal digits from start of the given byte slice.
+// If a non-hexdigit is met, parsing stops.
+// Return n - number of bytes read, and i - the value found.
+func Hex(b []byte) (i uint64, n int) {
+	for l := len(b); n < l; n++ {
+		if (b[n] - '0') < 10 {
+			i = (i * 16) + uint64(b[n]&0x0F)
+		} else if (b[n]&0xDF)-'A' < 6 {
+			i = (i * 16) + uint64(b[n]&0x0F) + 9
+		} else {
+			break
+		}
+	}
+	return
+}
+
+// Decimal parses all decimal digits from start of the given byte slice.
 // If a non-digit is met, parsing stops.
 // Return n - number of bytes read, and i - the value found.
-func Integer(b []byte) (i int64, n int) {
+func Decimal(b []byte) (i uint64, n int) {
 	l := len(b)
 	for ; n < l && (b[n]-'0') < 10; n++ {
-		i = 10*i + int64(b[n]&0x0f)
+		i = (i * 10) + uint64(b[n]&0x0F)
 	}
 	return
 }
@@ -34,7 +50,7 @@ func Float(b []byte) (f float64, n int) {
 		f += float64(fp) / float64(e)
 	}
 
-	if n >= l || (b[n]&0xdf) != 'E' {
+	if n >= l || (b[n]&0xDF) != 'E' {
 		return
 	}
 
@@ -53,5 +69,22 @@ func Float(b []byte) (f float64, n int) {
 		f *= math.Pow10(sign * exp)
 	}
 
+	return
+}
+
+// Number parses "digits" (0-9 and A-..., depending on base) from start of the given byte slice.
+// Parsing stop if a non-"digit" is met.
+// Use parse.Hex to parse known hexadecimals (is slightly faster)
+// Return n - number of bytes read, and i - the value found.
+func Number(b []byte, base uint64) (i uint64, n int) {
+	for bs, l := byte(base-10), len(b); n < l; n++ {
+		if (b[n] - '0') < 10 {
+			i = (i * base) + uint64(b[n]&0x0F)
+		} else if (b[n]&0xDF)-'A' < bs {
+			i = (i * base) + uint64(b[n]&0x0F) + 9
+		} else {
+			break
+		}
+	}
 	return
 }
--- parse/number_test.go
+++ parse/number_test.go
@@ -4,11 +4,46 @@ import (
 	"testing"
 )
 
-func TestInteger(t *testing.T) {
-	// func Integer(b []byte) (i int64, n int)
+func TestHex(t *testing.T) {
+	// func Hex(b []byte) (i uint64, n int)
 	tests := []struct {
 		input      string
-		expect     int64
+		expect     uint64
+		expectSize int
+	}{
+		{"0", 0, 1},
+		{"0a", 0x0A, 2},
+		{"0g", 0, 1},
+		{"0-", 0, 1},
+		{"1234", 0x1234, 4},
+		{"1234--", 0x1234, 4},
+
+		{"g012", 0, 0},
+		{"f012", 0xF012, 4},
+		{"f012g", 0xF012, 4},
+		{"F012g", 0xF012, 4},
+		{"A0g", 0xA0, 4},
+
+		{"3DA408B9", 0x3DA408B9, 8},
+		{"FFFFFFFF5B", 0xFFFFFFFF5B, 10},
+	}
+
+	for testID, test := range tests {
+		got, gotSize := Hex([]byte(test.input))
+		if got != test.expect {
+			t.Errorf("[test=%d] Expected [%v], got [%v]\n", testID, test.expect, got)
+		}
+		if gotSize != test.expectSize {
+			t.Errorf("[test=%d] Expected [%v], got [%v]\n", testID, test.expectSize, gotSize)
+		}
+	}
+}
+
+func TestInteger(t *testing.T) {
+	// func Integer(b []byte) (i uint64, n int)
+	tests := []struct {
+		input      string
+		expect     uint64
 		expectSize int
 	}{
 		{"0", 0, 1},
@@ -30,7 +65,7 @@ func TestInteger(t *testing.T) {
 }
 
 func TestFloat(t *testing.T) {
-	// func Integer(b []byte) (i int64, n int)
+	// Float(b []byte) (f float64, n int)
 	tests := []struct {
 		input      string
 		expect     float64
